53f729a56d5062a1271717d724980c57c9495666
Taven Shumaker <tavensshumaker@gmail.com>
2020-09-12 12:12:04 -0700

    Set up route handlers for login, sign-up and logout.

---
+++ server/controllers/cookieController.js
@@ -0,0 +1,5 @@
+
+//set ssid cookie
+//Occurs on login and sign up
+//adds user id (not username) to the ssid cookie
+//makes the cookie httpOnly (for security purposes)
---
+++ server/controllers/userController.js
@@ -0,0 +1,7 @@
+
+
+//create user middleware
+//check if username is already taken
+
+//verify user middleware
+//check if DB contains entry with matching username and password
